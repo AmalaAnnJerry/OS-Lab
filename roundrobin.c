81aaa8586d045c07df3b3058e2ae6c066e2bd72c
AmalaAnnJerry <119193067+AmalaAnnJerry@users.noreply.github.com>
2024-08-02 08:27:08 +0530

    Update roundrobin.c

--- roundrobin.c
+++ roundrobin.c
@@ -1,53 +1,120 @@
 #include<stdio.h>
- int main()
+
+int q[100];
+
+int f = -1;
+
+int r = -1;
+
+void insert (int n)
+
 {
- 
-  int cnt,j,n,t,remain,flag=0,tq;
-  int wt=0,tat=0,at[10],bt[10],rt[10];
-  printf("Enter Total Process:\t ");
-  scanf("%d",&n);
-  remain=n;
-  for(cnt=0;cnt<n;cnt++)
-  {
-    printf("Enter Arrival Time and Burst Time for Process Process Number %d :",cnt+1);
-    scanf("%d",&at[cnt]);
-    scanf("%d",&bt[cnt]);
-    rt[cnt]=bt[cnt];
-  }
-  printf("Enter Time Quantum:\t");
-  scanf("%d",&tq);
-  printf("\n\nProcess\t|Turnaround Time|Waiting Time\n\n");
-  for(t=0,cnt=0;remain!=0;)
-  {
-    if(rt[cnt]<=tq && rt[cnt]>0)
-    {
-      t+=rt[cnt];
-      rt[cnt]=0;
-      flag=1;
-    }
-    else if(rt[cnt]>0)
-    {
-      rt[cnt]-=tq;
-      t+=tq;
-    }
-    if(rt[cnt]==0 && flag==1)
-    {
-      remain--;
-      printf("P[%d]\t|\t%d\t|\t%d\n",cnt+1,t-at[cnt],t-at[cnt]-bt[cnt]);
-      wt+=t-at[cnt]-bt[cnt];
-      tat+=t-at[cnt];
-      flag=0;
-    }
-    if(cnt==n-1)
-      cnt=0;
-    else if(at[cnt+1]<=t)
-      cnt++;
-    else
-      cnt=0;
-  }
-  printf("\nAverage Waiting Time= %f\n",wt*1.0/n);
-  printf("Avg Turnaround Time = %f",tat*1.0/n);
-  return 0;
+
+if (f==-1)
+
+f=0;
+
+r = r+1;
+
+q[r] = n;
+
 }
 
+int delete ()
 
+{
+
+int n;
+
+n = q[f];
+
+f = f+1;
+
+return n;
+
+}
+
+void main()
+
+{
+
+int p, tq, n, i, t=0;
+
+float twt = 0, ttat = 0;
+
+printf ("\nEnter how many processes: ");
+
+scanf ("%d", &n);
+
+int a[n][5], bt[10], exist[10] = {0};
+
+printf ("\n Enter arrival time & burst time of each process:");
+
+for (i=0; i<n; i++)
+
+{
+
+printf ("\nProcess ID: ");
+
+scanf ("%d", &a[i][0]);
+
+printf ("\nA.T of process %d: ", a[i][0]);
+
+scanf ("%d", &a[i][1]);
+
+printf ("\nB.T of process %d: ", a[i][0]);
+
+scanf ("%d", &a[i][2]);
+
+bt[i] = a[i][2];
+
+}
+
+printf ("\nEnter time quantum: ");
+
+scanf ("%d", &tq);
+
+insert (0);
+
+exist[0] = 1;
+while (f<=r)
+{
+p = delete ();
+if (a[p][2]>=tq)
+{
+a[p][2]-=tq;
+t+=tq;
+}
+else
+{
+t+=a[p][2];
+a[p][2] = 0;
+}
+for (i=0; i<n; i++)
+{
+if (exist[i]==0 && a[i][1]<=t)
+{
+insert (i);
+exist[i]=1;
+}
+}
+if (a[p][2]==0)
+{
+a[p][4]=t-a[p][1];
+a[p][3]=a[p][4]-bt[p];
+ttat+=a[p][4];
+twt+=a[p][3];
+}
+else
+{
+insert (p);
+}
+}
+printf("\nP.No.\tAT\tBT\tTAT\tWT");
+for (i=0; i<n; i++)
+{
+printf("\n%d\t%d\t%d\t%d\t%d", a[i][0], a[i][1], bt[i], a[i][4], a[i][3]);
+}
+printf ("\nAverage Turnaround time = %f", ttat/n);
+printf ("\nAverage Waiting time = %f", twt/n);
+}
