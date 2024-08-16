a2b5ed15ac1537ab1cd60293abfc9bd67935add1
AmalaAnnJerry <119193067+AmalaAnnJerry@users.noreply.github.com>
2024-08-16 10:07:00 +0530

    Update seq filealloc.c

--- seq filealloc.c
+++ seq filealloc.c
@@ -1,61 +1,51 @@
-#include<stdio.h>
-#define FALSE 0
-#define TRUE 1
-
-int allocateSequentially(int startingPosition, int allocationLength, int maximumSize, int block[])
+#include <stdio.h>
+void main()
 {
-	int isPossible = TRUE;
-	int i; 
-    if(startingPosition + allocationLength > maximumSize + 1)
-    {
-        return FALSE;
-    }
-for(i=startingPosition-1; i<startingPosition+allocationLength-1; i++)
-    {
-    	if(block[i] == TRUE)
-    	{
-    		isPossible = FALSE;
-return FALSE;
-    	}
-    }
-    for(i=startingPosition-1; i<startingPosition+allocationLength-1; i++)
-    {
-    	block[i] = TRUE;
-    }
-    return TRUE;
+int n,flag[50], len[20], start[20], t[20], bno[20][20];
+int blockalloc[50]={0};
+printf("Enter no.of files:");
+scanf("%d", &n);
+for (int i = 0; i < n; i++)
+{
+printf("\nEnter the starting block of file.%d : ", i + 1);
+scanf("%d", &start[i]);
+printf("Enter length of file%d : ", i + 1);
+scanf("%d", &len[i]);
+flag[i]=1;
+for(int j=start[i];j<start[i]+len[i];j++)
+if(blockalloc[j]==1)
+{
+printf("Block.%d already allocated\n",j);
+flag[i]=0;
+break;
 }
-
-int main()
-{
-    int allocationLength, startingPosition, maximumSize;
-    int i, j;
-    int inputChoice;
-    printf("Enter the maximum size available : ");
-    scanf("%d", &maximumSize);
-    int block[maximumSize];
-    for(i=0; i<maximumSize; i++)
-    {
-    	block[i] = FALSE;
-    }
-    while(1)
-    {
-    	printf("\n\n\t\t MENU : \n\n\t\t 1. Allocate \n\n\t\t 2. Exit \n\n\t\t Choice : ");
-    	scanf("%d", &inputChoice);
-    	if(inputChoice == 2)
-    	{
-    		return 0;
-    	}
-    	printf("\n\n\t Enter the starting address : ");
-    	scanf("%d", &startingPosition);
-    	printf("\n\n\t Enter the length of the block : ");
-    	scanf("%d", &allocationLength);
-    	if(allocateSequentially(startingPosition, allocationLength, maximumSize, block) == TRUE)
-    	{
-    		printf("\n\n\t Successfully Allocated!");
-    	}
-    	else
-    	{
-    		printf("\n\n\t Space cannot be allocated!");
-    	}
- }
+if(flag[i]){
+for(int j=start[i];j<start[i]+len[i];j++){
+blockalloc[j]=1;
+}
+t[i] = start[i];
+for (int j = 0; j < len[i]; j++)
+bno[i][j] = t[i]++;
+printf("File allocated successfully\n");
+}
+}
+printf("FileNo\tStart\tlength\n");
+for (int i = 0; i < n; i++)
+if(flag[i])
+printf("%d\t\t %d \t\t%d\n", i + 1, start[i], len[i]);
+int choice,search;
+do{
+printf("\nEnter file No:");
+scanf("%d", &search);
+if(flag[search-1]){
+printf("File No : %d\n", search);
+printf("Length : %d\n", len[search - 1]);
+printf("blocks occupied:");
+for (int i = 0; i < len[search - 1]; i++)
+printf("%4d", bno[search - 1][i]);
+}
+else
+printf("File%d not allocated \n",search);
+printf("\nDo you want to search more (1/0) : ");scanf("%d",&choice);
+}while(choice);
 }
