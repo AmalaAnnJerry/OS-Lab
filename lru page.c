5a1250930b0e0f012d7855f6e2464360e07cf787
AmalaAnnJerry <119193067+AmalaAnnJerry@users.noreply.github.com>
2024-08-16 10:06:38 +0530

    Update lru page.c

--- lru page.c
+++ lru page.c
@@ -90,3 +90,46 @@ printf("Enter the number of frames: ");
 scanf("%d", &frame);
 LRU(pg,n,frame);
 }
+/* ALGORITHM
+1. Start
+2. Declare n, frame
+3. Read no of sequences in n and no of frames in frame
+4.Declare pg[n]
+5. Read each request and store it in pg[i]
+6. Use another function LRU and pass pg[],n,frame as argument
+//Inside LRU function
+7.Declare pagefault,hit, temp[frame],counter,time[frame],flag1.flag2,pos
+8. Initialise pagefault=0,hit=0,counter=0
+9. Add -1 to all entries in temp[] indicating initially all frames are free
+10. Now take each request as pg[i] and set flage1=flage2=0 and check if pg[i]
+already in temp if yes set counter++,time[j]=counter,flag1=flag2=1,hit++ and break
+Otherwise there are 2 options
+If flag1==0 and free frame exist then set temp[j]=pg[i]
+counter++,time[j]=counter , flag2=1 ,pagefault++ and print each temp[k] which is not
+-1 and break
+If flag2 still 0 means no free frame then first find least recently used frame
+using a function findLRU and store it in pos and set temp[pos]=pg[i], counter++,
+time[pos]=counter, pagefault++ and print each temp[k] which is not -1
+10.1 for i from 0 to n-1
+10.1.1 Set flag1=flag2=0 and print pg[i]
+10.1.2 for j from 0 to frame-1
+If temp[j]==pg[i] then set counter++ ,time[j]=counter hit++
+
+,flag1=flag2=1 and break
+
+10.1.3 if flag1==0 then
+For j from 0 to frame-1
+If temp[j]==-1 then set counter++ ,time[j]=counter pagefault++
+,flag2=1 and print temp[k] where k=0 to frame-1 and temp[k]!=-1 and break
+10.1.4 if flag2==0 then find LRU frame using findLRU()
+//inside findLRU()
+For i from 0 to n-1
+If time[i]<minimum set minimum=time[i] and pos=i
+Set temp[pos]=pg[i] , counter++,pagefault++,time[pos]=counter
+Print each temp[k] where k =0 to frame-1 and temp[k]!=-1
+
+11. Display Total page fault =pagefault
+12. Display total page hit as hit
+13. Display hit ratio as hit/n
+14. Stop
+  */
