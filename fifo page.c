b45379db8cf063f8d32abe2e7f46d3b0d4b4b51a
AmalaAnnJerry <119193067+AmalaAnnJerry@users.noreply.github.com>
2024-08-16 09:58:43 +0530

    Update fifo page.c

--- fifo page.c
+++ fifo page.c
@@ -52,3 +52,32 @@ printf("Enter the number of frames: ");
 scanf("%d", &frame);
 FIFO(pg,n,frame);
 }
+/* ALGORITHM
+1. Start
+2. Declare n, frame
+3. Read no of sequences in n and no of frames in frame
+4.Declare pg[n]
+5. Read each request and store it in pg[i]
+6. Use another function FIFO and pass pg[],n,frame as argument
+//Inside FIFO function
+7.Declare pagefault,hit,k,flag,temp[frame]
+8. Initialise pagefault=0,hit=0,k=0
+9. Add -1 to all entries in temp[] indicating initially all frames are free
+10. Now take each request ie pg[i] then set flag=0 then check if pg[i] is already
+present in temp if yes set flag to 1 , hit++ and print “Hit” otherwise set temp[k] to pg[i]
+where k denotes the frame in which oldest request is present and set k to
+k+1%frame and pagefault++ and then print all entries of temp which are not -1
+10.1 for i from 0 to n-1
+10.1.1 Set flag=0 and print pg[i]
+10.1.2 for j from 0 to frame-1
+If temp[j]==pg[i] then set hit++,flag=1 and print hit
+10.1.3 if flag==0 then
+Set temp[k]=pg[i] and k=(k+1)%frame
+For j from 0 to frame-1
+If temp[j]!=-1 then print temp[j]
+
+11. Display Total page fault =pagefault
+12. Display total page hit as hit
+13. Display hit ratio as hit/n
+14. Stop
+*/
